39042c8f32ceaa9f78f3f6e9d33d3e9f1b76ff96
Devin Hard <devin.hard@promega.com>
2020-10-07 19:01:46 -0500

    rewrote whole v1 script to use functions and config file

---
+++ AU_Downloaderv2.ps1
@@ -0,0 +1,97 @@
+function main {
+    $config = get-confiugration
+    foreach ($repo in $config.items){
+        $repoData = get-repoData $repo
+        #Write-Host "repo data debug"
+        #write-host $repoData.files
+        #write-host $repoData
+        #pause
+        if ($repoData.version -ne $repo.version ){                           # check if version is different
+            get-filedownload -fileList $repoData -folder $repo.folder   # downloads the files and writes them to disk
+            #Write-Host $repo.version
+            $repo.version = $repoData.version                                # This will set the value of the repo version to the current version here and in the $config varaible
+            #Write-Host $repo.version
+            #pause
+        }
+        #update config file / write to disk
+        
+    }
+    update-config $config
+}
+
+function get-confiugration {
+    try {
+        return Get-Content parameters.json | ConvertFrom-Json
+    }
+    catch [System.Management.Automation.ItemNotFoundException] {
+        # if not found create a new configuration file
+    }
+    
+}
+
+function get-repoData {
+    param(
+        [psobject]$repoConfigData
+    )
+    $repo = $repoConfigData.repository
+    $repo_URL = "https://api.github.com/repos/$repo/releases"
+    #Write-Host $repo_URL
+    try {
+        $repoData = (Invoke-WebRequest $repo_URL | ConvertFrom-Json)[0]
+    }
+    catch [System.Net.WebException]{
+        Write-Error "Network issue, repository is not accessible"
+    }
+    return parse-repoItems -repoItems $repoData
+}
+
+function parse-repoItems {
+    param(
+        [psobject]$repoItems
+    )
+    $data = New-Object -TypeName psobject
+    $data | Add-Member -MemberType NoteProperty -Name "version" -Value $repoItems.id
+    $data | Add-Member -MemberType NoteProperty -Name "files" -Value @()
+    #write-host "repo full object"
+    foreach ($item in $repoItems.assets) {
+        #Write-Host "browser url in items"
+        #write-host $item.browser_download_url
+        $repoValues = New-Object -TypeName psobject
+        $repoValues | Add-Member -MemberType NoteProperty -Name download_url -Value $item.browser_download_url
+        $repoValues | Add-Member -MemberType NoteProperty -Name filename -Value $item.name
+        $data.files += $repoValues
+    }
+    return $data
+}
+
+function get-filedownload {
+    param (
+        [psobject]$fileList,
+        [string]$folder
+    )
+    if($folder -eq ""){
+        $path = ""
+    }
+    else {
+        $path = $folder + "\"
+    }
+    foreach($file in $fileList.files){
+        if($file.filename -ne "final.txt"){
+            $temp_path = $path + $file.filename
+            $request = $file.download_url
+            #Write-Host $file.download_url
+            Invoke-WebRequest $request -OutFile $temp_path
+        }
+    }
+}
+
+function update-config {
+    param (
+        [psobject]$config
+    )
+    Move-Item parameters.json parameters.json.back -Force
+    $config | ConvertTo-Json | out-file -FilePath parameters.json
+    
+}
+
+main
