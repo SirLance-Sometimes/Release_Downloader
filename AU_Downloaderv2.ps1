a7e591f1d325b853ef28adb1722925e65e65bf75
Devin Hard <devin.hard@promega.com>
2020-10-09 20:21:01 -0500

    Wrote a function to build a config file if it is missing

--- AU_Downloaderv2.ps1
+++ AU_Downloaderv2.ps1
@@ -33,6 +33,27 @@ function get-confiugration {
     
 }
 
+function publish-config {
+    $template = @{
+        "items" = @(
+            @{
+                name = "";
+                repository = "";
+                folder = "";
+                version = 0;
+                excludeFiles = @()
+            }
+        )
+    }
+
+    $template | ConvertTo-Json | Out-File parameter.json
+    Write-Host "No configuration file was found, a template configuration file was built as parameter.json"
+    Write-Host "Populate parameter.json with repo information to use this program"
+    Write-Host "Program is exiting, rerun after paramters.json is populated"
+    Write-Host "Press enter to exit"
+    Pause
+    exit
+}
 function get-repoData {
     param(
         [psobject]$repoConfigData
