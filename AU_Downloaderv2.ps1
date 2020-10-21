caa27fe37fcecd174be0a849d9c5cd80656baca3
SirLance-Sometimes <dhard91@gmail.com>
2020-10-20 19:59:19 -0500

    Tested and fixed issues with write debug

--- AU_Downloaderv2.ps1
+++ AU_Downloaderv2.ps1
@@ -1,3 +1,5 @@
+[Cmdletbinding()]
+param()
 $logfile = "repo_download.log"
 $StartTime = get-date -Format 'yyyy-mm-dd hh:mm:ss.fff'
 Start-Transcript -Path $logfile -Append -NoClobber
@@ -5,20 +7,19 @@ Write-Information "$StartTime Script started"
 function main {
     $config = get-confiugration
     foreach ($repo in $config.items){
-        log -level 2 -message ("checking repo: " + $repo.repository)
+        Write-information "checking repo: $($repo.repository)"
         $repoData = get-repoData $repo
-        Write-debug "repo data debug"
-        #write-debug "Repo files:" + $repoData.files
-        #write-debug "Repo data:" + $repoData
+        write-debug "Repo files: $($repoData.files)"
+        write-debug "Repo data: $($repoData)"
         if ($repoData.version -ne $repo.version ){                           # check if version is different
-            #Write-Information $repo.repository + " has been updated."
+            Write-Information "$($repo.repository) has been updated"
             get-filedownload -fileList $repoData -folder $repo.folder -exclude $repo.exclude  # downloads the files and writes them to disk
-            #Write-debug "repo version before update:" + $repo.version
+            Write-debug "repo version before update: $($repo.version)"
             $repo.version = $repoData.version                                # This will set the value of the repo version to the current version here and in the $config varaible
-            #Write-Debug "repo version after update: " + $repo.version
+            Write-Debug "repo version after update: $($repo.version)"
         }
         else {
-            #Write-Information $repo.repository + " has not been updated."
+            Write-Information "$($repo.repository) has not been updated."
         }
     }
     update-config $config
@@ -131,8 +132,9 @@ function update-config {
 }
 
 main
+Write-Information $DebugPreference
 $EndTime = Get-Date -Format 'yyyy-mm-dd hh:mm:ss.fff'
 Write-information "$EndTime Script finished"
-$TimeSpan = New-TimeSpan -Start $StartTime -End $EndTime
-Write-information "Script runtime was $($TimeSpan.Hours) hours, $($TimeSpan.Minutes) minutes, $($TimeSpan.Seconds) seconds."
+#$TimeSpan = New-TimeSpan -Start $StartTime -End $EndTime
+#Write-information "Script runtime was $($TimeSpan.Hours) hours, $($TimeSpan.Minutes) minutes, $($TimeSpan.Seconds) seconds."
 Stop-Transcript
