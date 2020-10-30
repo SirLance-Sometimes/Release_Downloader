80ffa0b9910dc50261f6786fbbab95d1d82df898
SirLance-Sometimes <dhard91@gmail.com>
2020-10-30 14:34:25 -0500

    Split publish-config

--- AU_Downloaderv2.ps1
+++ AU_Downloaderv2.ps1
@@ -27,27 +27,6 @@ function main {
     update-config $config
 }
 
-function publish-config {
-    $template = @{
-        "items" = @(
-            @{
-                name = "";
-                repository = "";
-                folder = "";
-                version = 0;
-                excludeFiles = @()
-            }
-        )
-    }
-
-    $template | ConvertTo-Json | Out-File parameters.json
-    Write-Host "No configuration file was found, a template configuration file was built as parameter.json"
-    Write-Host "Populate parameter.json with repo information to use this program"
-    Write-Host "Program is exiting, rerun after paramters.json is populated"
-    Write-Host "Press enter to exit"
-    Pause
-    exit
-}
 function get-repoData {
     param(
         [psobject]$repoConfigData
