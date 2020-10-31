fcb96f97fef14a14f89f6eabf39fc5beb6de1cc6
SirLance-Sometimes <dhard91@gmail.com>
2020-10-31 10:29:39 -0500

    Split get-fileDownload

---
+++ get-fileDownload/get-fileDownload.psm1
@@ -0,0 +1,26 @@
+function get-fileDownload {
+    param (
+        [psobject]$fileList,
+        [string]$folder,
+        [string]$exclude,
+        [string]$filters
+    )
+    if($folder -eq ""){
+        $path = ""
+    }
+    else {
+        $path = $folder + "\"
+    }
+    Write-Debug "get-fileDownload $fileList.files $($fileList.files)"
+    foreach($file in $fileList.files){
+        #todo This should be a check agaisn't a list from the configuration file, an exclude list.
+        Write-Debug "get-fileDownload file.filename $($file.filename)"
+        Write-Debug "get-fileDownload exclude $($exclude)"
+        if( $exclude -notcontains $file.filename -and $file.filename -eq ( $file.filename | Select-String -Pattern $filters ) ){
+            $temp_path = $path + $file.filename
+            $request = $file.download_url
+            Write-Debug "get-fileDownload file.download_url $($file.download_url)"
+            Invoke-WebRequest $request -OutFile $temp_path
+        }
+    }
+}
