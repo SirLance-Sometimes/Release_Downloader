9d4f956de3a142e97fcb61815bdced6b04df69c7
SirLance-Sometimes <dhard91@gmail.com>
2020-10-31 16:15:07 -0500

    Renamed function files to .ps1

---
+++ get-configuration/get-configuration.ps1
@@ -0,0 +1,17 @@
+#Requires -Modules publish-config
+function get-configuration {
+    param(
+        [string]$configPath
+    )
+    try {
+        $content = Get-Content $configPath -ErrorAction Stop | ConvertFrom-Json
+        Write-Information "Configuration file parameters.json found and loaded"
+        return $content
+    }
+    catch [System.Management.Automation.ItemNotFoundException] {
+        # if not found create a new configuration file
+        Write-Information "Configuration file not found"
+        publish-config
+    }
+    
+}
