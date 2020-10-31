456cd0d60319773582c202c1cffa552368a68e53
SirLance-Sometimes <dhard91@gmail.com>
2020-10-31 10:23:02 -0500

    Split update-config

--- AU_Downloaderv2.ps1
+++ AU_Downloaderv2.ps1
@@ -8,6 +8,7 @@ $ProgressPreference = 'SilentlyContinue'
 function get-imports {
     Import-Module .\publish-config
     Import-Module .\get-confiugration
+    Import-Module .\update-config
     
 }
 function main {
@@ -109,15 +110,7 @@ function cleanup-files {
     }
 }
 
-function update-config {
-    param (
-        [psobject]$config
-    )
-    write-debug "update-config config.items[0].exclude $($config.items[0].exclude)"
-    write-debug "update-config config as json $(ConvertTo-Json $config)"
-    ConvertTo-Json $config | out-file -FilePath parameters.json
-    
-}
+
 
 main
 Write-Information $DebugPreference
---
+++ update-config/update-config.psm1
@@ -0,0 +1,9 @@
+function update-config {
+    param (
+        [psobject]$config
+    )
+    write-debug "update-config config.items[0].exclude $($config.items[0].exclude)"
+    write-debug "update-config config as json $(ConvertTo-Json $config)"
+    ConvertTo-Json $config | out-file -FilePath parameters.json
+    
+}
