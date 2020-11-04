e711ba35f7e5e43e510669d32577c16c5fa70108
SirLance-Sometimes <dhard91@gmail.com>
2020-11-03 21:06:48 -0600

    Fix write-host to write-output

--- src/publish-config.ps1
+++ src/publish-config.ps1
@@ -12,10 +12,10 @@ function publish-config {
     }
 
     $template | ConvertTo-Json | Out-File parameters.json
-    Write-Host "No configuration file was found, a template configuration file was built as parameter.json"
-    Write-Host "Populate parameter.json with repo information to use this program"
-    Write-Host "Program is exiting, rerun after paramters.json is populated"
-    Write-Host "Press enter to exit"
+    Write-Output "No configuration file was found, a template configuration file was built as parameter.json"
+    Write-Output "Populate parameter.json with repo information to use this program"
+    Write-Output "Program is exiting, rerun after paramters.json is populated"
+    Write-Output "Press enter to exit"
     Pause
     exit
 }
