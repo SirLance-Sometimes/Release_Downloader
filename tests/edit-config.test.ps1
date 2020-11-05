b96b3c721b46e3d3c83d3ae0323db167eb4956da
SirLance-Sometimes <dhard91@gmail.com>
2020-11-04 19:57:11 -0600

    Moved to tests folder

--- tests/edit-config.test.ps1
+++ tests/edit-config.test.ps1
@@ -1,9 +1,9 @@
 Describe 'edit-config' {
-    . ./edit-config.ps1
+    . ./src\edit-config.ps1
     Set-Location "TestDrive:\"
     edit-config -config @{items=@(@{name="test1";exclude="test1Exclude"},@{name="test2";exclude="test2Exclude"})}
     It 'writes parameters.json file' {
         "TestDrive:\parameters.json" | Should Exist
     }
-    Set-Location $psscriptroot
+    Set-Location $psscriptroot\..
 }
