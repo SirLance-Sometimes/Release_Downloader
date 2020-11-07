fae503d0354129be5c3e5a972b8187d1c921b9ff
SirLance-Sometimes <dhard91@gmail.com>
2020-11-06 19:43:52 -0600

    Remove pause and exit for unit testing

--- src/publish-config.ps1
+++ src/publish-config.ps1
@@ -15,7 +15,7 @@ function publish-config {
     Write-Output "No configuration file was found, a template configuration file was built as parameter.json"
     Write-Output "Populate parameter.json with repo information to use this program"
     Write-Output "Program is exiting, rerun after paramters.json is populated"
-    Write-Output "Press enter to exit"
-    Pause
-    exit
+    #Write-Output "Press enter to exit"
+    #Pause
+    #exit
 }
